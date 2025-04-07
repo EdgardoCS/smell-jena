e2b78afd91001974b60aa1afd17cf15046e8826b
EdgardoCS <e.silva.plaza@gmail.com>
2025-04-07 17:23:14 +0200

    added requirements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-author:
+author: Antonie Bierling @FSU Jena
 editor: EdgardoCS @FSU Jena
 date: 24/03/2025
 """
@@ -12,7 +12,7 @@ import seaborn as sns
 import numpy as np
 
 
-def countInside(area, areaRange):
+def count_inside(area, areaRange):
     """
 
     :param area: surface area to extend the search
@@ -20,15 +20,15 @@ def countInside(area, areaRange):
     :return: points (in number) and image to be saved
     """
     match = []
-    targetData = area
+    target_data = area
     x = areaRange[0]
     y = areaRange[1]
     w = areaRange[2]
     h = areaRange[3]
 
-    for i in range(0, len(targetData)):
-        if x <= targetData.iloc[i, 3] < x + w and y <= targetData.iloc[i, 2] < y + h:
-            match.append([int(targetData.iloc[i, 3]), int(targetData.iloc[i, 2])])
+    for i in range(0, len(target_data)):
+        if x <= target_data.iloc[i, 3] < x + w and y <= target_data.iloc[i, 2] < y + h:
+            match.append([int(target_data.iloc[i, 3]), int(target_data.iloc[i, 2])])
     match = np.array(match)
 
     count = len(match)
@@ -37,58 +37,57 @@ def countInside(area, areaRange):
     plt.show()
     return count
 
+if __name__ == "__main__":
 
-questionnaire = pd.read_excel("data/smell_behavior_sociodemographics.xlsx")
+    # Load data from questionnaire
+    questionnaire = pd.read_excel("data/smell_behavior_sociodemographics.xlsx")
+    # Load self and other body odor dataframes
+    self = pd.read_excel("data/body_silhouettes_self.xlsx")
+    other = pd.read_excel("data/body_silhouettes_other.xlsx")
 
-# Extract valid entries (id) from cleaned questionnaire dataframe
-included = questionnaire["id"].unique()
+    # Extract valid entries (id) from cleaned questionnaire dataframe and filter for valid entries
+    included = questionnaire["id"].unique()
+    self = self[self['id'].isin(included)]
+    other = other[other['id'].isin(included)]
 
-# Load self and other body odor dataframes
-self = pd.read_excel("data/body_silhouettes_self.xlsx")
-other = pd.read_excel("data/body_silhouettes_other.xlsx")
+    # Group by id and count the number of rows for each id.
+    # This will count how many points each subject marked, and store that value on self-agg
+    # The same for other_agg
+    self_count = self.groupby("id").size().reset_index(name='count')
+    self_agg = pd.merge(self[["country", "id"]], self_count, on="id").drop_duplicates()
 
-# Filter for valid entries
-self = self[self['id'].isin(included)]
-other = other[other['id'].isin(included)]
+    # Group by id and count the number of rows for each id
+    other_count = other.groupby("id").size().reset_index(name='count')
+    other_agg = pd.merge(other[["country", "id"]], other_count, on="id").drop_duplicates()
+    # other_agg
 
-# Group by id and count the number of rows for each id
-self_count = self.groupby("id").size().reset_index(name='count')
-self_agg = pd.merge(self[["country", "id"]], self_count, on="id").drop_duplicates()
-# self_agg
+    # df = other
+    df = other.loc[other["country"] == "HKG"]  # to choose a particular country
 
-# Group by id and count the number of rows for each id
-other_count = other.groupby("id").size().reset_index(name='count')
-other_agg = pd.merge(other[["country", "id"]], other_count, on="id").drop_duplicates()
-# other_agg
+    # Load image
+    map_img = mpimg.imread('data/humanbody_clear.png')  # change path to image path
 
-# df = other
-df = other.loc[other["country"] == "HKG"]  # to choose a particular country
+    # Plot image
+    hmax = sns.scatterplot(x="other_x", y="other_y", data=df, alpha=0.08, zorder=2)
+    hmax.imshow(map_img)
+    hmax.invert_yaxis()
+    hmax.invert_xaxis()
 
-# Load image
-map_img = mpimg.imread('data/humanbody_clear.png')  # change path to image path
+    hmax.set_xlim(hmax.get_xlim()[::-1])
+    hmax.set_ylim(hmax.get_ylim()[::-1])
 
-# Plot image
-hmax = sns.scatterplot(x="other_x", y="other_y", data=df, alpha=0.08, zorder=2)
+    # Find segments
+    # Load body segments
+    segments = pd.read_excel("data/body_segments.xlsx")
+    for i in range(0, len(segments)):
+        included = [1,2]
+        if segments.iloc[i]["id_segment"] in included:
+            rectangleRange = [segments.iloc[i]['x'], segments.iloc[i]['y'], segments.iloc[i]['w'], segments.iloc[i]['h']]
+            matchedPoints = count_inside(df, rectangleRange)
+            print("segment has", matchedPoints, "points")
 
-hmax.imshow(map_img)
-hmax.invert_yaxis()
-hmax.invert_xaxis()
-
-hmax.set_xlim(hmax.get_xlim()[::-1])
-hmax.set_ylim(hmax.get_ylim()[::-1])
-
-# Find segments
-# Load body segments
-segments = pd.read_excel("data/body_segments.xlsx")
-for i in range(0, len(segments)):
-    included = [1,2]
-    if segments.iloc[i]["id_segment"] in included:
-        rectangleRange = [segments.iloc[i]['x'], segments.iloc[i]['y'], segments.iloc[i]['w'], segments.iloc[i]['h']]
-        matchedPoints = countInside(df, rectangleRange)
-        print("segment has", matchedPoints, "points")
-
-plt.axis("on")
-# plt.savefig("bodysilhouette_other.png", dpi=300, format="png")
-print("Body odor perceived from body of others")
-plt.show()
-# print("           front ----------------------------- back")
+    plt.axis("on")
+    # plt.savefig("bodysilhouette_other.png", dpi=300, format="png")
+    print("Body odor perceived from body of others")
+    plt.show()
+    # print("           front ----------------------------- back")
