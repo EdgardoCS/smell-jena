ff23e02a9cc9c1b4ca4f79755be183bc66242563
EdgardoCS <e.silva.plaza@gmail.com>
2025-04-11 15:01:18 +0200

    ready to share

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ editor: EdgardoCS @FSU Jena
 date: 24/03/2025
 """
 
+import math
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+
+
+def truncate(number, digits) -> float:
+    # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
+    nbDecimals = len(str(number).split('.')[1])
+    if nbDecimals <= digits:
+        return number
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
 
 
 def count_whole(data, rectangle_range):
@@ -118,46 +128,72 @@ if __name__ == "__main__":
     whole_body = ["front_side", "back_side"]
     segments_location = ["front", "back"]
 
-    country_front = []
-    country_back = []
-    country_whole_front = []
-    count_whole_back = []
+    fig, axes = plt.subplots(nrows=2, ncols=9)
+    axes = axes.flatten()
 
     for i, country in enumerate(countries):
         df = other.loc[other["country"] == country]
 
-        if i < 2:
+        whole_front = 0
+        whole_back = 0
+        front_points = 0
+        back_points = 0
 
-            print("Sorting data...", country)
-            for segment in whole_body:
-                rectangle = segments.set_index('segment').loc[segment, ['location', 'x', 'y', 'w', 'h']]
-                whole_front, whole_back = count_whole(df, rectangle)
+        print("Sorting data...", country)
+        rectangle = segments.set_index('segment').loc["front_side", ['location', 'x', 'y', 'w', 'h']]
+        whole_front, whole_back = count_whole(df, rectangle)
 
-            for segment in segments_names:
-                rectangle = segments.set_index('segment').loc[segment, ['location', 'x', 'y', 'w', 'h']]
-                front_points, back_points = count_inside(df, rectangle)
+        for segment in segments_names:
+            rectangle = segments.set_index('segment').loc[segment, ['location', 'x', 'y', 'w', 'h']]
+            front_points, back_points = count_inside(df, rectangle)
 
-                front_points = len(front_points)
-                back_points = len(back_points)
-                f_percentage = front_points * 100 / whole_front
-                b_percentage = back_points * 100 / whole_back
+            front_points = len(front_points)
+            back_points = len(back_points)
 
-                new_data = {
-                    "frontal_percentage": [f_percentage],
-                    "back_percentage": [b_percentage]
-                }
-                new_df = pd.DataFrame(new_data)
+            # print(front_points)
+            # print(back_points)
+            f_percentage = truncate(front_points * 100 / whole_front, 1)
+            b_percentage = truncate(back_points * 100 / whole_back, 1)
 
-                # segments = segments.reset_index()
+            new_data = {
+                "frontal_percentage": [f_percentage],
+                "back_percentage": [b_percentage]
+            }
+            new_df = pd.DataFrame(new_data)
 
-                segments.loc[
-                    (segments['segment'] == segment) & (segments['location'] == "front"),
-                    ['percentage']] = [f_percentage]
+            # segments = segments.reset_index()
 
-                segments.loc[
-                    (segments['segment'] == segment) & (segments['location'] == "back"),
-                    ['percentage']] = [b_percentage]
+            segments.loc[
+                (segments['segment'] == segment) & (segments['location'] == "front"),
+                ['percentage']] = [f_percentage]
+
+            segments.loc[
+                (segments['segment'] == segment) & (segments['location'] == "back"),
+                ['percentage']] = [b_percentage]
 
         new_segments = segments[segments["percentage"].notnull()]
-        print(new_segments)
 
+        hmax = sns.scatterplot(data=new_segments, x="xc", y="yc", size="percentage", sizes=(300, 400), legend=False,
+                               ax=axes[i], alpha=0.5)
+        for line in range(0, 37):
+            try:
+                hmax.text(
+                    new_segments["xc"][line] + 0.1,
+                    new_segments["yc"][line] + 0.2,
+                    new_segments["percentage"][line],
+                    ha="left",
+                    weight="bold"
+                )
+            except KeyError:
+                pass
+        hmax.imshow(map_img)
+        hmax.invert_yaxis()
+        hmax.invert_xaxis()
+
+        hmax.set_xlim(hmax.get_xlim()[::-1])
+        hmax.set_ylim(hmax.get_ylim()[::-1])
+        axes[i].axis("off")
+        axes[i].set_title(country)
+
+    fig.delaxes(axes[17])
+    plt.show()
